b3eca2d3ac99ab5d3642471de6eae9ea40bee7ed
steefg3000 <steef.gijsbers@gmail.com>
2022-07-11 21:54:39 +0200

    Install Semantic-UI

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,9 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import 'jquery'
+import '@doabit/semantic-ui-sass'
+import '@doabit/semantic-ui-sass/src/scss/semantic-ui.scss'
 
 Rails.start()
 Turbolinks.start()
