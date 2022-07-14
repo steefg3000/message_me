b0311def322a8256b5b356eec8577d024bef1109
steefg3000 <steef.gijsbers@gmail.com>
2022-07-14 16:38:48 +0200

    Create websocket functionality - java scrolling does not work

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,6 +11,12 @@ import 'jquery'
 import '@doabit/semantic-ui-sass'
 // import '@doabit/semantic-ui-sass/src/scss/semantic-ui.scss'
 
+// scroll_bottom = function() {
+//   if ($('#messages').length > 0) {
+//     $('#messages').scrollTop($('#messages')[0].scrollHeight);
+//   }
+// }
+
 $(document).on('turbolinks:load', function () {
   $('.ui.dropdown').dropdown();
 
@@ -22,6 +28,7 @@ $(document).on('turbolinks:load', function () {
         ;
     })
     ;
+    // scroll_bottom();
 })
 
 
